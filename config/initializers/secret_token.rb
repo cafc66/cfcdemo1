7801bab15c78e4d1f68e0f722a090f315a23b7f9
Carlos Ferraro Cavallini <cafc66@gmail.com>
2011-08-29 12:59:35 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cfcdemo1::Application.config.secret_token = '1b08fbd6c06b36c75f284892fca07e3cfac34613445361ce5a639912d9d2e8ce171472f3c1c42cba608409d2918d2f99a2464f7f6ab31c400eaf666961a54b7b'
